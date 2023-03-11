0b863eae4dd04d62f3c0f699fae8f4ff88134ceb
DarkSwarp <113368991+DarkSwarp@users.noreply.github.com>
2023-03-11 12:25:21 +0000

    Deploying to gh-pages from @ DarkSwarp/goit-js-hw-10@aaea4caa017f3adc66d9598af8d366fb9895ecd1 🚀

--- index.29d1e641.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={fetchCountries:function(t){return fetch("".concat("https://restcountries.com/v3.1/name/").concat(t)).then((function(t){if(!t.ok)throw new Error(t.status);return t.json()}))}};var n="";document.querySelector("input").addEventListener("keyup",(function(e){n=e.currentTarget.value,t.fetchCountries(n).then((function(t){})).catch((function(t){console.log(t)}))}))}();
-//# sourceMappingURL=index.29d1e641.js.map
--- index.ff77e1f1.js
+++
@@ -1,2 +0,0 @@
-var e={fetchCountries:function(e){return fetch(`https://restcountries.com/v3.1/name/${e}`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))}};let t="";document.querySelector("input").addEventListener("keyup",(r=>{t=r.currentTarget.value,e.fetchCountries(t).then((e=>{})).catch((e=>{console.log(e)}))}));
-//# sourceMappingURL=index.ff77e1f1.js.map
